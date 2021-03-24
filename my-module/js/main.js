83d0edb940bcdd7f961294d1f08ce1c44d4b2adc
dennis <dennis@mail.com>
2021-03-24 16:54:52 +0200

    first commit

---
+++ my-module/js/main.js
@@ -0,0 +1,5 @@
+$('.slider').slick({
+    arrows: true,
+    prevArrow: '<div class="slider-arrow left-button "> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"/> </svg> </div>',
+    nextArrow: '<div class="slider-arrow right-button "> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"/> </svg> </div>',
+});
